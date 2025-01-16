6f5bed4c0f00e1469381892baacd9d059c5a50be
Rohit singh thakur <rrajput2060@gmail.com>
2025-01-17 02:33:39 +0530

    commit it

--- Config/database.js
+++ Config/database.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    const { connection } = await mongoose.connect("mongodb+srv://rrajput2060:Kq3NITgS3dVcVHQs@cluster0.oa0fm.mongodb.net/courseBit");
+    const { connection } = await mongoose.connect("mongodb+srv://rohit:Rohit@5698@cluster0.3yt0s.mongodb.net/Amazon?retryWrites=true");
   console.log(`MongoDB connected with ${connection.host}`);
   } catch (error) {
     console.log(error)
--- utils/sendToken.js
+++ utils/sendToken.js
@@ -1,5 +1,4 @@
 export const sendToken = (res, user, message, statusCode = 200) => {
-console.log('first')
     const token = user.getJWTToken();
     const options = {
       expires: new Date(Date.now() + 15 * 24 * 60 * 60 * 1000),
