62965e43ec19ebae7b28a253636ecc54e5952e5d
Rohit singh thakur <rrajput2060@gmail.com>
2025-01-17 02:37:42 +0530

    changes

--- Config/database.js
+++ Config/database.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    const { connection } = await mongoose.connect("mongodb+srv://rohit:Rohit@5698@cluster0.3yt0s.mongodb.net/Amazon?retryWrites=true");
+    const { connection } = await mongoose.connect("mongodb+srv://rohit:Rohit@5698@cluster0.3yt0s.mongodb.net/And?retryWrites=true");
   console.log(`MongoDB connected with ${connection.host}`);
   } catch (error) {
     console.log(error)
